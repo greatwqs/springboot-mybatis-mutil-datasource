b785f7f203ae3c401d7cf6ef086221b9cba44594
wangqingsong <greatwqs@gmail.com>
2019-09-21 09:33:23 +0800

    本地测试OK; http://localhost:8080/api/user?userName=11

--- src/main/java/org/spring/springboot/Application.java
+++ src/main/java/org/spring/springboot/Application.java
@@ -2,8 +2,6 @@ package org.spring.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Spring Boot 应用启动类
@@ -12,13 +10,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  */
 // Spring Boot 应用的标识
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer{
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
-	}
-
+public class Application {
 	public static void main(String[] args) {
 		// 程序启动入口
 		// 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
--- src/main/java/org/spring/springboot/controller/UserRestController.java
+++ src/main/java/org/spring/springboot/controller/UserRestController.java
@@ -11,27 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 用户控制层
- *
+ * <p>
  * Created by bysocket on 07/02/2017.
  */
 @RestController
 public class UserRestController {
 
-	@Autowired //@Resource
-	private UserService userService;
+    @Autowired //@Resource
+    private UserService userService;
 
-	/**
-	 * 根据用户名获取用户信息，包括从库的地址信息
-	 *
-	 * @param userName
-	 * @return
-	 */
-	@RequestMapping(value = "/api/user", method = RequestMethod.GET)
-	public User findByName(Model mode,
-		@RequestParam(value = "userName", required = true) String userName) {
-		//		mode.
-		//		mode.getClass().isAnnotationPresent(Autowired.class);
-		return userService.findByName(userName);
-	}
+    /**
+     * 根据用户名获取用户信息，包括从库的地址信息
+     *
+     * @param userName
+     * @return
+     */
+    @RequestMapping(value = "/api/user", method = RequestMethod.GET)
+    public User findByName(Model mode, @RequestParam(value = "userName", required = true) String userName) {
+
+        return userService.findByName(userName);
+    }
 
 }
